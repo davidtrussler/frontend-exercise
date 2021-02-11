e3ada58e7661158a8901405d427abb7fdea6852e
David Trussler <mail@davidtrussler.net>
2021-02-11 15:00:36 +0000

    Gets main and associated articles - Adds article page template - Updates router to deal with requests for articles

--- server/router.js
+++ server/router.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const article = require('./content/article.json'); 
+const posts = require('./content/posts.json'); 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	res.render("home", {
+	res.render('home', {
 		meta: req.app.get('meta')
 	});
+});
+
+router.get(/\/article\//, (req, res) => {
+	let articleName = req.url.split('/')[2]; // this isn't used at the minute
+	let data = {
+		article: article, 
+		posts: posts
+	};
+
+	res.render('article', data); 
 });
 
 module.exports = router;
